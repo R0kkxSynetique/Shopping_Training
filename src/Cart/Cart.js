494c9ddcc8374897d9d2423f70375c19607b30fa
Benjamin Fontana <ben192003@gmail.com>
2024-02-01 11:15:45 +0100

    feat: add total getter

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,23 +1,28 @@
-const EmptyCartException = require('./EmptyCartException');
-
+const EmptyCartException = require("./EmptyCartException");
 
 module.exports = class Cart {
+	_items = [];
 
-    _items = [];
+	constructor(items) {
+		this._items = items;
+	}
 
-    constructor(items) {
-        this._items = items;
-    }
+	get items() {
+		this.#checkEmptyCart();
+		return this._items;
+	}
 
-    get items() {
-        this.#checkEmptyCart();
-        return this._items;
-    }
+	get total() {
+		let total = 0;
+		this._items.forEach((item) => {
+			total += item.total;
+		});
+		return total;
+	}
 
-    #checkEmptyCart() {
-        if (this._items == null) {
-            throw new EmptyCartException();
-        }
-    }
-
+	#checkEmptyCart() {
+		if (this._items == null) {
+			throw new EmptyCartException();
+		}
+	}
 };
